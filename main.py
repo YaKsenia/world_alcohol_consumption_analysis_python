e8cc320e0ea81ddb159cc7502f5efc6905306147
Ksenia Yakovleva <xenia.iakovleva@gmail.com>
2019-12-27 01:01:48 +0100

    added comments to the script

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+This script processes the data, explores the data (EDA), computes descriptive statistics of them 
+and conducts hypothesis tests for each type of alcohol
+
+'''
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,68 +12,116 @@ from statistical_tests import hypothesis_test, ecdf, draw_bs_pairs, permutation_
 
 #pd.set_option('display.max_rows', df.shape[0]+1)
 
-df = pd.read_csv('alco_merge_full.csv')
+#read the file 
+df = pd.read_csv('data_world_alcohol_consumption.csv')
 
-print('Original size of data:', len(df))
+print('Number of rows and columns of the data:', df.shape)
+
+#keep only the columns which are necessary for analysis
 
 df = df[['Country', 'Beverage Types','2015', '2016']]
 
-test_df = df
-test_df['diff'] = df['2016'] - df['2015']
-test_df = test_df[test_df['2016'] != 0.0]
-test_df = test_df[test_df['2015'] != 0.0]
-test_df['raise'] = np.where(test_df['diff'] > 0, 1, 0)
-test_df = test_df.dropna()
+#create a copy of the original dataframe
+compare = df.copy()
 
-print('Number of countries when alcohol consumption in 2016 raised:', len(test_df[test_df['raise'] == 1]))
-print('Number of countries when alcohol consumption in 2016 dropped or didn\'t change:', len(test_df[test_df['raise'] == 0]), '\n')
+#create a column with differences between 2016 and 2015 alcohol consumption value
+compare['diff'] = compare['2016'] - compare['2015']
 
+#keep only values which are more than 0
+compare = compare[compare['2016'] != 0.0]
+compare = compare[compare['2015'] != 0.0]
+
+#create a column which tells if the alcohol consumption raised (1) or didn't raise (0)
+compare['raise'] = np.where(compare['diff'] > 0, 1, 0)
+compare = compare.dropna()
+
+print('Number of countries where alcohol consumption in 2016 raised:', len(compare[compare['raise'] == 1]))
+print('Number of countries where alcohol consumption in 2016 dropped or didn\'t change:', len(compare[compare['raise'] == 0]), '\n')
+
+#create new columns "Year" and 'Value' instead of two columns 2015 and 2016
 df = df.melt(id_vars=['Country', 'Beverage Types'], 
         var_name="Year", 
         value_name="Value")
 
+#remove rows where alcohol consumption value is 0
 df = df[df['Value'] != 0.0]
 
-types = ['All types', 'Beer', 'Wine', 'Spirits']
+#create a dataframe where the type of alcohol is only "All types"
 
 all_types = df[df['Beverage Types'] == 'All types']
 
+print('Statistics of the "Value" column: ', '\n', all_types.Value.describe(), '\n')
+
+#delete rows with null values
+
 all_types = all_types.loc[all_types.Value.notnull()]
 
+#group the dataframe by Country and calculate sum of values for each country
+
 country = all_types.groupby('Country').sum().reset_index()
+
+#choose 10 countries where the sum of alcohol consumption for 2015-2016 was the highest
+
 alco_leaders = country.nlargest(10, 'Value', keep='first')
+
 print('Countries leaders in alcohol consumption:', '\n', alco_leaders, '\n')
+
+#create a list of 10 most 'alcoholic' countries'names
+
 leaders_list = list(alco_leaders['Country'])
 
+#use this list to choose from the main df only the rows with these countries
+
 leaders = df.loc[df['Country'].isin(leaders_list)]
+
+#group by country name and year (so we get every country's alcohol value twice - for 2015 and 2016
 leaders = leaders.groupby(['Country', 'Year']).mean().reset_index()
-leaders = leaders.sort_values(by='Value')
+#leaders = leaders.sort_values(by='Value')
 
 #sns.set_color_codes("pastel")
+
+#create a pivot table, so country becomes an index
 pivot = pd.pivot_table(leaders,  values='Value',  columns=['Year'],  
                          index = "Country", aggfunc=np.sum,  fill_value=0)
+
+#sort values by 2016 alcohol value
 pivot = pivot.reindex(pivot.sort_values(by=['2016'], ascending=False).index)
+
+#create a visualization plot
 pivot.plot(kind="bar")
 #plt.show()
 
+plt.tight_layout()
+#plt.savefig('countries_leaders_2015-2016.png' , dpi=300)
+plt.close()
+
+types = ['All types', 'Beer', 'Wine', 'Spirits']
+
+#go through every type of alcohol in the data and analyse it separately
 for alco_type in types:
 
 	print("Analysis for world consumption of this type of alcohol:", alco_type)
 	print('\n')
 
+	#make a new temporary dataframe with only this type of alcohol
 	new_df = df[df['Beverage Types'] == alco_type]
 	new_df = new_df.dropna()
 
 	print('Sample size: ', len(new_df))
 
+	#make two different dataframes for 2015 and 2016
 	data2015 = new_df[new_df['Year'] == '2015']
 	data2016 = new_df[new_df['Year'] == '2016']
 	
+	#calculate descriptive statistics
 	print('Sample mean for 2015: ', data2015['Value'].mean())
 	print('Sample mean for 2016: ', data2016['Value'].mean())
 
 	print('Sample median for 2015: ', data2015['Value'].median())
 	print('Sample median for 2016: ', data2016['Value'].median())
+
+	print('Sample STD for 2015: ', data2015['Value'].std())
+	print('Sample STD for 2016: ', data2016['Value'].std())
 
 	print('Minumum value for 2015: ', data2015['Value'].min())
 	print('Minumum value for 2016: ', data2016['Value'].min())
@@ -76,7 +129,8 @@ for alco_type in types:
 	print('Maximum value for 2015: ', data2015['Value'].max())
 	print('Maximum value for 2016: ', data2016['Value'].max())
 
-	# Compute ECDFs
+	# Compute ECDFs for 2015 and 2016
+
 	x_2015, y_2015 = ecdf(list(data2015.Value))
 	x_2016, y_2016 = ecdf(list(data2016.Value))
 
@@ -93,10 +147,15 @@ for alco_type in types:
 	_ = plt.ylabel('ECDF')
 	_ = plt.legend(('2015','2016'), loc='lower right')
 
-	# Show the plot
+	# Show the plot or save it to a file (ucomment what you want to do)
+
 	#plt.savefig('ecdf_compare2015-2016_' + alco_type + '.png' , dpi=200)
 	#plt.show()
 	plt.close()
+
+	# Compute two hypothesis tests - based on bootstrap replicates and permutation replicates
+	# The hypothesis is: World alcohol consumption in 2016 raised significatly comparing to 2015
+	# Null hypothesis: There was no significant change in world alcohol consumption
 
 	print('\n', 'Hypothesis test for', alco_type, '\n')
 	
@@ -113,7 +172,6 @@ for alco_type in types:
 	# Compute 95% confidence interval: conf_int
 	conf_int = np.percentile(bs_diff_replicates, [2.5, 97.5])
 
-	# Print the results
 	print('difference of means =', mean_diff)
 	print('95% confidence interval for differences of means =', conf_int)
 
@@ -121,13 +179,13 @@ for alco_type in types:
 
 	print('P-value with bootstrap replicates: ', p_value)
 
-	empirical_diff_means = diff_of_means(x_2015, x_2016)
+	mean_diff_permutation = diff_of_means(x_2015, x_2016)
 
 	# Draw 10,000 permutation replicates: perm_replicates
 	perm_replicates = draw_perm_reps(x_2015, x_2016, diff_of_means, size=10000)
 
 	# Compute p-value: p
-	p_value2 = np.sum(perm_replicates <= empirical_diff_means) / len(perm_replicates)
+	p_value2 = np.sum(perm_replicates <= mean_diff_permutation) / len(perm_replicates)
 
 	# Print the result
 	print('P-value with permutation replicates:', p_value2)
