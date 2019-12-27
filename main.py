4cbd11e3575afb91a9c1c3e90882f3765dad5de2
Ksenia Yakovleva <xenia.iakovleva@gmail.com>
2019-12-27 15:16:17 +0100

    added overview of alcohol consumption through years and visualization of it at the beginning

--- main.py
+++ main.py
@@ -10,13 +10,62 @@ import seaborn as sns
 from draw_bootstrap_replications import bootstrap_replicate_1d, draw_bs_reps
 from statistical_tests import hypothesis_test, ecdf, draw_bs_pairs, permutation_sample, draw_perm_reps, diff_of_means
 
-#pd.set_option('display.max_rows', df.shape[0]+1)
-
 #read the file 
 
 df = pd.read_csv('data_world_alcohol_consumption.csv')
-
+#pd.set_option('display.max_rows', df.shape[0]+1)
 print('Number of rows and columns of the data:', df.shape)
+
+
+#Exploratory data analysis
+
+overview = df.copy()
+
+#create new columns "Year" and 'Value' instead of all the columns with years
+
+overview = overview.melt(id_vars=['Unnamed: 0', 'Country', 'Data Source_x', 'Beverage Types'], 
+        var_name="Year", 
+        value_name="Value")
+
+#delete the rows with null values
+overview = overview.loc[overview.Value.notnull()]
+
+#keep the rows with data for all types of alcohol
+overview = overview[overview['Beverage Types'] == 'All types']
+
+#remove the outlier: this years has data only for 2 countries, while other years - for more than 200
+overview = overview[overview['Year'] != '1960']
+
+#group the data by year and calculate mean value for each year
+overview = overview.groupby(by=['Year']).mean().reset_index()
+
+#convert year column to integer datatype
+overview.Year = overview.Year.astype('int')
+
+sns.set_color_codes("pastel")
+
+#visualize changes of alcohol consumption means for every year
+
+_ = plt.plot(overview["Year"], overview["Value"], color='r')
+
+plt.margins(0.02)
+
+# Add axis labels and legend
+_ = plt.xlabel('Year')
+_ = plt.ylabel('Alcohol consumption value')
+
+
+#save the graph as png image or show (uncomment the option which you prefer)
+
+#plt.savefig('overview_alcohol_consumption_all_years.png' , dpi=300)
+#plt.show()
+
+
+'''
+
+Analysis with comparison of alcohol consumption between two years: 2015 and 2016
+
+'''
 
 #keep only the columns which are necessary for the analysis
 
